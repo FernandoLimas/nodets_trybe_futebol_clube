5f5424881185a6ac3078f2d3978a7dce3d1aa466
fernandolima <nando_filipe@hotmail.com>
2022-05-04 17:08:30 -0300

    refactor: model/Matches

--- app/backend/src/database/models/Matches.ts
+++ app/backend/src/database/models/Matches.ts
@@ -1,23 +1,55 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class Matches extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  Matches.init({
-    home_team: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Matches',
-  });
-  return Matches;
-};
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+import Teams from './Teams';
+// import OtherModel from './OtherModel';
+
+class Matches extends Model {
+  // public <campo>!: <tipo>;
+  public id!: number;
+  public homeTeam!: number;
+  public homeTeamGoals!: number;
+  public awayTeam!: number;
+  public awayTeamGoals!: number;
+  public inProgress!: number;
+}
+
+Matches.init({
+  // ... Campos
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  homeTeam: {
+    type: DataTypes.INTEGER,
+  },
+  homeTeamGoals: {
+    type: DataTypes.INTEGER,
+  },
+  awayTeam: {
+    type: DataTypes.INTEGER,
+  },
+  awayTeamGoals: {
+    type: DataTypes.INTEGER,
+  },
+  inProgress: {
+    type: DataTypes.INTEGER,
+  }
+}, {
+  // ... Outras configs
+  underscored: true,
+  sequelize: db,
+  // modelName: 'example',
+  modelName: 'Matches',
+  timestamps: false,
+});
+
+// ---associations---
+Matches.belongsTo(Teams, { foreignKey: 'homeTeam', as: 'teamHome' });
+Matches.belongsTo(Teams, { foreignKey: 'awayTeam', as: 'teamAway' });
+
+Teams.hasMany(Matches, { foreignKey: 'homeTeam', as: 'HomeMatch' });
+Teams.hasMany(Matches, { foreignKey: 'awayTeam', as: 'AwayMatch' });
+
+
+export default Matches;
