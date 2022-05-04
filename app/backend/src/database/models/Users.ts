2c95868e68e16dfe01a967a0d4137745503774dd
fernandolima <nando_filipe@hotmail.com>
2022-05-04 15:42:30 -0300

    refactor: models/Users

---
+++ app/backend/src/database/models/Users.ts
@@ -0,0 +1,47 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+// import OtherModel from './OtherModel';
+
+class User extends Model {
+  // public <campo>!: <tipo>;
+  public id!: number;
+  public username!: string;
+  public role!: string;
+  public email!: string;
+  public password!: string;
+}
+
+User.init({
+  // ... Campos
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+    
+}, {
+  // ... Outras configs
+  underscored: true,
+  sequelize: db,
+  // modelName: 'example',
+  modelName: 'User',
+  timestamps: false,
+});
+
+export default User;
