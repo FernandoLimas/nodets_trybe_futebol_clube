4b38576d25b21c1f885726aedb38cc4e0868ef82
fernandolima <nando_filipe@hotmail.com>
2022-05-18 16:39:06 -0300

    feat: express.json()

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import { Touchscreen } from 'puppeteer';
 
 class App {
   public app: express.Express;
@@ -21,6 +22,7 @@ class App {
     };
 
     this.app.use(accessControl);
+    this.app.use(express.json());
     // ...
   }
 
