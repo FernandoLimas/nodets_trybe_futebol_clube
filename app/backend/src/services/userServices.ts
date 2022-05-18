5df2b547eeb62c62c4d472d1befcee4aae49c70b
fernandolima <nando_filipe@hotmail.com>
2022-05-18 18:38:50 -0300

    feat: userService

---
+++ app/backend/src/services/userServices.ts
@@ -0,0 +1,11 @@
+import * as bcrypt from 'bcrypt';
+import Users from '../database/models/Users';
+
+export default class LoginService {
+  public static async createUser(email: string, password: string) {
+    const hasUser = await Users.findOne({ where: { email } });
+    if (!hasUser) {
+      const hash = bcrypt.hashSync(password, 10);
+    }
+  }
+}
