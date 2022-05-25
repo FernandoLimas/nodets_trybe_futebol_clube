e8512c758df4ba89afd797be39c3a20cc9b4e65c
fernandolima <nando_filipe@hotmail.com>
2022-05-25 00:51:37 -0300

    feat: service leaderBoards and business rules

---
+++ app/backend/src/services/LeaderBoard.ts
@@ -0,0 +1,61 @@
+// business rules
+import Teams from '../database/models/Teams';
+import Matches from '../database/models/Matches';
+import ScoreBoard from './ScoreBoard';
+
+export default class ScoreBoardService {
+  private sortResults = (results: ScoreBoard[]) => {
+    const orderByGoalsFavor = results.sort((a, b) => b.goalsFavor - a.goalsFavor);
+    const orderByBalance = orderByGoalsFavor.sort((a, b) => b.goalsBalance - a.goalsBalance);
+    const orderByVictories = orderByBalance.sort((a, b) => b.totalVictories - a.totalVictories);
+    const orderByPoints = orderByVictories.sort((a, b) => b.totalPoints - a.totalPoints);
+
+    return orderByPoints;
+  };
+
+  public async home() {
+    const allMatches = await Matches.findAll();
+    const allTeams = await Teams.findAll();
+    const results: ScoreBoard[] = []; // initial empty
+    allTeams.forEach((team) => {
+      const teste = allMatches
+        .filter((match) => match.homeTeam === team.id && match.inProgress === false) //  home team and match finish
+        .map((match) => ({ goalsFavor: match.homeTeamGoals, goalsOwn: match.awayTeamGoals })); // goals favor and goals own
+      const result = new ScoreBoard({ teamName: team.teamName, matches: teste });
+      results.push(result);
+    });
+    return this.sortResults(results);
+  }
+
+  public async away() {
+    const allMatches = await Matches.findAll();
+    const allTeams = await Teams.findAll();
+    const results: ScoreBoard[] = [];
+    allTeams.forEach((team) => {
+      const teste = allMatches
+        .filter((match) => match.awayTeam === team.id && match.inProgress === false)
+        .map((match) => ({ goalsFavor: match.awayTeamGoals, goalsOwn: match.homeTeamGoals }));
+      const result = new ScoreBoard({ teamName: team.teamName, matches: teste });
+      results.push(result);
+    });
+    return this.sortResults(results);
+  }
+
+  public async allGames() {
+    const allMatches = await Matches.findAll();
+    const allTeams = await Teams.findAll();
+    const results: ScoreBoard[] = [];
+    allTeams.forEach((team) => {
+      let homeGames = allMatches
+        .filter((match) => match.homeTeam === team.id && match.inProgress === false)
+        .map((match) => ({ goalsFavor: match.homeTeamGoals, goalsOwn: match.awayTeamGoals }));
+      const awayGames = allMatches
+        .filter((match) => match.awayTeam === team.id && match.inProgress === false)
+        .map((match) => ({ goalsFavor: match.awayTeamGoals, goalsOwn: match.homeTeamGoals }));
+      homeGames = [...homeGames, ...awayGames];
+      const result = new ScoreBoard({ teamName: team.teamName, matches: homeGames });
+      results.push(result);
+    });
+    return this.sortResults(results);
+  }
+}
